660c17e5c7552c0c91bd177c5318cbde7fb71805
vauxhall <zero.griffin@gmail.com>
2016-01-22 16:22:20 -0600

    mpic

---
+++ graphuss/sourceteq/source/m/pic/mpic.h
@@ -0,0 +1,13 @@
+//
+//  mpic.h
+//  graphuss
+//
+//  Created by zero on 1/22/16.
+//  Copyright © 2016 Iturbide. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface mpic : NSObject
+
+@end
---
+++ graphuss/sourceteq/source/m/pic/mpic.m
@@ -0,0 +1,13 @@
+//
+//  mpic.m
+//  graphuss
+//
+//  Created by zero on 1/22/16.
+//  Copyright © 2016 Iturbide. All rights reserved.
+//
+
+#import "mpic.h"
+
+@implementation mpic
+
+@end
