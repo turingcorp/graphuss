3d622854f0d75c567d0ba99836be90bba657f13f
vauxhall <zero.griffin@gmail.com>
2016-01-24 20:35:07 -0600

    pic item temp

---
+++ graphuss/sourceteq/source/m/pic/mpicitemtemp.h
@@ -0,0 +1,13 @@
+//
+//  mpicitemtemp.h
+//  graphuss
+//
+//  Created by zero on 1/24/16.
+//  Copyright © 2016 Iturbide. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface mpicitemtemp : NSObject
+
+@end
---
+++ graphuss/sourceteq/source/m/pic/mpicitemtemp.m
@@ -0,0 +1,13 @@
+//
+//  mpicitemtemp.m
+//  graphuss
+//
+//  Created by zero on 1/24/16.
+//  Copyright © 2016 Iturbide. All rights reserved.
+//
+
+#import "mpicitemtemp.h"
+
+@implementation mpicitemtemp
+
+@end
