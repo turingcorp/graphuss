8e8b87499169887aca8107cb0ba99c0058095c82
vauxhall <zero.griffin@gmail.com>
2016-01-25 18:06:22 -0600

    textfield

--- graphuss/sourceteq/source/v/pic/vpicdetaileditscale.m
+++ graphuss/sourceteq/source/v/pic/vpicdetaileditscale.m
@@ -40,7 +40,7 @@
     UITextField *fieldwidth = [[UITextField alloc] init];
     [fieldwidth setAutocapitalizationType:UITextAutocapitalizationTypeNone];
     [fieldwidth setAutocorrectionType:UITextAutocorrectionTypeNo];
-    [fieldwidth setBackgroundColor:[UIColor colorWithWhite:1 alpha:0.4]];
+    [fieldwidth setBackgroundColor:[UIColor colorWithWhite:1 alpha:0.3]];
     [fieldwidth setBorderStyle:UITextBorderStyleNone];
     [fieldwidth setClearButtonMode:UITextFieldViewModeNever];
     [fieldwidth setDelegate:self];
@@ -53,12 +53,14 @@
     [fieldwidth setTextColor:[UIColor blackColor]];
     [fieldwidth setTintColor:[UIColor blackColor]];
     [fieldwidth setTranslatesAutoresizingMaskIntoConstraints:NO];
+    [fieldwidth setClipsToBounds:YES];
+    [fieldwidth.layer setCornerRadius:4];
     self.fieldwidth = fieldwidth;
     
     UITextField *fieldheight = [[UITextField alloc] init];
     [fieldheight setAutocapitalizationType:UITextAutocapitalizationTypeNone];
     [fieldheight setAutocorrectionType:UITextAutocorrectionTypeNo];
-    [fieldheight setBackgroundColor:[UIColor colorWithWhite:1 alpha:0.4]];
+    [fieldheight setBackgroundColor:[UIColor colorWithWhite:1 alpha:0.3]];
     [fieldheight setBorderStyle:UITextBorderStyleNone];
     [fieldheight setClearButtonMode:UITextFieldViewModeNever];
     [fieldheight setDelegate:self];
@@ -71,16 +73,20 @@
     [fieldheight setTextColor:[UIColor blackColor]];
     [fieldheight setTintColor:[UIColor blackColor]];
     [fieldheight setTranslatesAutoresizingMaskIntoConstraints:NO];
+    [fieldheight setClipsToBounds:YES];
+    [fieldheight.layer setCornerRadius:4];
     self.fieldheight = fieldheight;
     
     UIButton *buttonaccept = [[UIButton alloc] init];
-    [buttonaccept setBackgroundColor:[UIColor colorWithWhite:0 alpha:0.5]];
+    [buttonaccept setBackgroundColor:[UIColor colorWithWhite:0 alpha:0.3]];
     [buttonaccept.layer setCornerRadius:4];
     [buttonaccept setClipsToBounds:YES];
     [buttonaccept setTitle:NSLocalizedString(@"pic_detail_scale_btnaccept", nil) forState:UIControlStateNormal];
     [buttonaccept setTitleColor:[UIColor whiteColor] forState:UIControlStateNormal];
     [buttonaccept setTitleColor:[UIColor colorWithWhite:1 alpha:0.1] forState:UIControlStateHighlighted];
     [buttonaccept setTranslatesAutoresizingMaskIntoConstraints:NO];
+    [buttonaccept addTarget:self action:@selector(actionaccept:) forControlEvents:UIControlEventTouchUpInside];
+    [buttonaccept.titleLabel setFont:[UIFont fontWithName:fontboldname size:17]];
     
     self.buttonaccept = buttonaccept;
     
@@ -92,6 +98,8 @@
     [buttoncancel setTitleColor:[UIColor whiteColor] forState:UIControlStateNormal];
     [buttoncancel setTitleColor:[UIColor colorWithWhite:1 alpha:0.1] forState:UIControlStateHighlighted];
     [buttoncancel setTranslatesAutoresizingMaskIntoConstraints:NO];
+    [buttoncancel addTarget:self action:@selector(actioncancel:) forControlEvents:UIControlEventTouchUpInside];
+    [buttoncancel.titleLabel setFont:[UIFont fontWithName:fontboldname size:17]];
     
     self.buttoncancel = buttoncancel;
     
@@ -108,13 +116,25 @@
     [self addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"V:|-0-[blur]-0-|" options:0 metrics:metrics views:views]];
     [self addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"H:|-50-[fieldwidth]-50-|" options:0 metrics:metrics views:views]];
     [self addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"H:|-50-[fieldheight]-50-|" options:0 metrics:metrics views:views]];
-    [self addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"V:|-100-[fieldwidth(70)]-0-[fieldheight(70)]-50-[accept(44)]-20-[cancel(44)]" options:0 metrics:metrics views:views]];
+    [self addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"V:|-100-[fieldwidth(55)]-40-[fieldheight(55)]-50-[accept(46)]-20-[cancel(46)]" options:0 metrics:metrics views:views]];
     [self addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"H:|-50-[accept]-50-|" options:0 metrics:metrics views:views]];
     [self addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"H:|-50-[cancel]-50-|" options:0 metrics:metrics views:views]];
     
     [self print];
     
     return self;
+}
+
+#pragma mark actions
+
+-(void)actionaccept:(UIButton*)button
+{
+    
+}
+
+-(void)actioncancel:(UIButton*)button
+{
+    [self animate:NO];
 }
 
 #pragma mark functionality
@@ -132,6 +152,13 @@
      ^
      {
          [self setAlpha:alpha];
+     } completion:
+     ^(BOOL done)
+     {
+         if(!show)
+         {
+             [self removeFromSuperview];
+         }
      }];
 }
 
@@ -139,6 +166,16 @@
 {
     [self.fieldwidth setText:[[tools singleton] numbertostring:@(width)]];
     [self.fieldheight setText:[[tools singleton] numbertostring:@(height)]];
+}
+
+#pragma mark -
+#pragma mark field del
+
+-(BOOL)textFieldShouldReturn:(UITextField*)field
+{
+    [field resignFirstResponder];
+    
+    return YES;
 }
 
 @end
