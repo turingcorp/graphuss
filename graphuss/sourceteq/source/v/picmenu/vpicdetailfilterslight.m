b60e74a5a04f9d4d675dd8dd5de1aa5993d98c7b
vauxhall <zero.griffin@gmail.com>
2016-01-27 15:36:35 -0600

    lighttype

--- graphuss/sourceteq/source/v/picmenu/vpicdetailfilterslight.m
+++ graphuss/sourceteq/source/v/picmenu/vpicdetailfilterslight.m
@@ -1,5 +1,12 @@
 #import "vpicdetailfilterslight.h"
 
+typedef NS_ENUM(NSInteger, lighttype)
+{
+    lighttypenone,
+    lighttypelight,
+    lighttypedark
+};
+
 @implementation vpicdetailfilterslight
 
 -(instancetype)init
@@ -20,9 +27,9 @@
     UISlider *slider = [[UISlider alloc] init];
     [slider setTranslatesAutoresizingMaskIntoConstraints:NO];
     [slider setTintColor:colormain];
-    [slider setMaximumTrackTintColor:colormain];
-    [slider setMinimumTrackTintColor:[UIColor colorWithRed:0.3 green:0.6 blue:0.8 alpha:1]];
-//    [slider addTarget:self action:@selector(actionslider:) forControlEvents:UIControlEventValueChanged];
+    [slider setMaximumTrackTintColor:[UIColor colorWithRed:0.3 green:0.6 blue:0.8 alpha:1]];
+    [slider setMinimumTrackTintColor:[UIColor blackColor]];
+    [slider addTarget:self action:@selector(actionslider:) forControlEvents:UIControlEventValueChanged];
     [slider setMinimumValue:-1];
     [slider setMaximumValue:1];
     [slider setValue:0 animated:NO];
@@ -40,9 +47,18 @@
     [self addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"H:|-0-[border]-0-|" options:0 metrics:metrics views:views]];
     [self addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"V:|-0-[border(1)]" options:0 metrics:metrics views:views]];
     [self addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"H:|-50-[slider]-50-|" options:0 metrics:metrics views:views]];
-    [self addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"V:|-40-[slider]" options:0 metrics:metrics views:views]];
+    [self addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"V:|-20-[slider]" options:0 metrics:metrics views:views]];
     
     return self;
+}
+
+#pragma mark actions
+
+-(void)actionslider:(UISlider*)slider
+{
+    CGFloat value = slider.value;
+    
+    
 }
 
 @end
