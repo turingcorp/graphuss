084e6aac14c4b9fa080fbaf756dbb08a42c6448d
vauxhall <zero.griffin@gmail.com>
2016-01-28 20:35:21 -0600

    view preview

---
+++ graphuss/sourceteq/source/v/picmenu/vpicdetailpreview.h
@@ -0,0 +1,9 @@
+#import "appdel.h"
+
+@class vpicdetail;
+
+@interface vpicdetailpreview:UIView
+
+-(instancetype)init:(vpicdetail*)detail;
+
+@end
---
+++ graphuss/sourceteq/source/v/picmenu/vpicdetailpreview.m
@@ -0,0 +1,12 @@
+#import "vpicdetailpreview.h"
+
+@implementation vpicdetailpreview
+
+-(instancetype)init:(vpicdetail*)detail
+{
+    self = [super init];
+
+    return self;
+}
+
+@end
