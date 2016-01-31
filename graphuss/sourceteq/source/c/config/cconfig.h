02f52a19d4789984ec93ee17e4c832ff8f76aef5
vauxhall <zero.griffin@gmail.com>
2016-01-31 16:28:06 -0600

    cconfig

--- graphuss/sourceteq/source/c/config/cconfig.h
+++ graphuss/sourceteq/source/c/config/cconfig.h
@@ -1,13 +1,5 @@
-//
-//  cconfig.h
-//  graphuss
-//
-//  Created by zero on 1/31/16.
-//  Copyright © 2016 Iturbide. All rights reserved.
-//
+#import "appdel.h"
 
-#import <Foundation/Foundation.h>
-
-@interface cconfig : NSObject
+@interface cconfig:UIViewController
 
 @end
