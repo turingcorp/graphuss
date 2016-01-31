52f860ce93e645e52c1ca3b4de4d5b0b27808b62
vauxhall <zero.griffin@gmail.com>
2016-01-31 00:49:30 -0600

    typo

--- graphuss/sourceteq/source/m/picmenu/mpicmenufilterscoloronlyblack.m
+++ graphuss/sourceteq/source/m/picmenu/mpicmenufilterscoloronlyblack.m
@@ -14,7 +14,7 @@
 
 -(id<mgraphicsfilterprotocol>)filter
 {
-    mgraphicsfilterbwbasic *filter = [[mgraphicsfilterbwbasic alloc] init];
+    mgraphicsfilterbwbasic *filter = [[mgraphicsfiltercoloronlyred alloc] init];
     
     return filter;
 }
