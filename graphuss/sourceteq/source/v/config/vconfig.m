0b4c89f38cce3b399512a3da5435792859b127ab
vauxhall <zero.griffin@gmail.com>
2016-01-31 16:29:24 -0600

    vconfig

---
+++ graphuss/sourceteq/source/v/config/vconfig.m
@@ -0,0 +1,13 @@
+//
+//  vconfig.m
+//  graphuss
+//
+//  Created by zero on 1/31/16.
+//  Copyright © 2016 Iturbide. All rights reserved.
+//
+
+#import "vconfig.h"
+
+@implementation vconfig
+
+@end
