864ef1c9b4ef8cec6041079f2619cd5ef82ff70c
vauxhall <zero.griffin@gmail.com>
2016-01-30 18:31:13 -0600

    cleaned contrast

--- graphuss/sourceteq/source/m/graphics/mgraphicsfilterbwcontrast.m
+++ graphuss/sourceteq/source/m/graphics/mgraphicsfilterbwcontrast.m
@@ -9,21 +9,31 @@
 {
     uint newcolor;
     uint sum = pixel.red255 + pixel.green255 + pixel.blue255;
+    uint delta = 10;
+    uint uaverage;
     NSInteger average = round(sum / 3.0);
     
-    if(average < 246)
+    if(average > 127)
     {
-        if(average > 127)
-        {
-            average += 10;
-        }
-        else if(average > 9)
-        {
-            average -= 10;
-        }
+        average += delta;
+    }
+    else
+    {
+        average -= delta;
     }
     
-    newcolor = [mgraphicspixel intfromintsred:average green:average blue:average];
+    if(average > 255)
+    {
+        average = 255;
+    }
+    else if(average < 0)
+    {
+        average = 0;
+    }
+    
+    uaverage = (uint)average;
+    
+    newcolor = [mgraphicspixel intfromintsred:uaverage green:uaverage blue:uaverage];
     
     return newcolor;
 }
