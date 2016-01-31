fc8266a25afdcf9d8d7989e03a155794abb24dcb
vauxhall <zero.griffin@gmail.com>
2016-01-30 19:15:35 -0600

    color only black

---
+++ graphuss/sourceteq/source/m/picmenu/mpicmenufilterscoloronlyblack.m
@@ -0,0 +1,13 @@
+//
+//  mpicmenufilterscoloronlyblack.m
+//  graphuss
+//
+//  Created by zero on 1/30/16.
+//  Copyright © 2016 Iturbide. All rights reserved.
+//
+
+#import "mpicmenufilterscoloronlyblack.h"
+
+@implementation mpicmenufilterscoloronlyblack
+
+@end
