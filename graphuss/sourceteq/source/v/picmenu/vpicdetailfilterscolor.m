962f047d0a9a0bbe0ad42d90f688458406c80837
vauxhall <zero.griffin@gmail.com>
2016-01-30 21:11:31 -0600

    collection

--- graphuss/sourceteq/source/v/picmenu/vpicdetailfilterscolor.m
+++ graphuss/sourceteq/source/v/picmenu/vpicdetailfilterscolor.m
@@ -14,19 +14,47 @@
     
     vblur *blur = [vblur light:NO];
     
-    [self addSubview:blur];
+    UICollectionViewFlowLayout *flow = [[UICollectionViewFlowLayout alloc] init];
+    [flow setHeaderReferenceSize:CGSizeZero];
+    [flow setMinimumInteritemSpacing:0];
+    [flow setMinimumLineSpacing:0];
+    [flow setScrollDirection:UICollectionViewScrollDirectionHorizontal];
     
-    NSDictionary *views = @{@"blur":blur};
+    UICollectionView *collection = [[UICollectionView alloc] initWithFrame:CGRectZero collectionViewLayout:flow];
+    [collection setClipsToBounds:YES];
+    [collection setBackgroundColor:[UIColor clearColor]];
+    [collection setShowsHorizontalScrollIndicator:NO];
+    [collection setShowsVerticalScrollIndicator:NO];
+    [collection setAlwaysBounceHorizontal:YES];
+    [collection setDataSource:self];
+    [collection setDelegate:self];
+    [collection setTranslatesAutoresizingMaskIntoConstraints:NO];
+    [collection registerClass:[vpicdetailfilterscolorcel class] forCellWithReuseIdentifier:celid];
+    [collection registerClass:[vpicdetailfilterscolorfooter class] forSupplementaryViewOfKind:UICollectionElementKindSectionFooter withReuseIdentifier:footerid];
+    
+    [self addSubview:blur];
+    [self addSubview:collection];
+    
+    NSDictionary *views = @{@"blur":blur, @"col":collection};
     NSDictionary *metrics = @{};
     
     [self addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"H:|-0-[blur]-0-|" options:0 metrics:metrics views:views]];
     [self addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"V:|-0-[blur]-0-|" options:0 metrics:metrics views:views]];
+    [self addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"H:|-0-[col]-0-|" options:0 metrics:metrics views:views]];
+    [self addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"V:|-0-[col]-0-|" options:0 metrics:metrics views:views]];
     
     return self;
 }
 
 #pragma mark -
 #pragma mark col del
+
+-(CGSize)collectionView:(UICollectionView*)col layout:(UICollectionViewLayout*)layout referenceSizeForFooterInSection:(NSInteger)section
+{
+    CGSize size = CGSizeMake(col.bounds.size.width, 100);
+    
+    return size;
+}
 
 -(NSInteger)numberOfSectionsInCollectionView:(UICollectionView*)col
 {
@@ -38,6 +66,20 @@
     NSInteger count = [self.model count];
     
     return count;
+}
+
+-(UICollectionReusableView*)collectionView:(UICollectionView*)col viewForSupplementaryElementOfKind:(NSString*)kind atIndexPath:(NSIndexPath*)index
+{
+    vpicdetailfilterscolorfooter *footer = [col dequeueReusableSupplementaryViewOfKind:kind withReuseIdentifier:footerid forIndexPath:index];
+    
+    return footer;
+}
+
+-(UICollectionViewCell*)collectionView:(UICollectionView*)col cellForItemAtIndexPath:(NSIndexPath*)index
+{
+    vpicdetailfilterscolorcel *cel = [col dequeueReusableCellWithReuseIdentifier:celid forIndexPath:index];
+    
+    return cel;
 }
 
 @end
