61e1b6212bb942badb1f9db9b0ce6caea1b08318
vauxhall <zero.griffin@gmail.com>
2016-01-31 16:35:12 -0600

    spinner

--- graphuss/sourceteq/source/v/config/vconfig.m
+++ graphuss/sourceteq/source/v/config/vconfig.m
@@ -2,4 +2,23 @@
 
 @implementation vconfig
 
+-(instancetype)init:(cconfig*)controller
+{
+    self = [super init:controller];
+    [self setClipsToBounds:YES];
+    [self setBackgroundColor:[UIColor whiteColor]];
+    
+    vspinner *spinner = [[vspinner alloc] init];
+    
+    [self addSubview:spinner];
+    
+    NSDictionary *views = @{@"spin":spinner};
+    NSDictionary *metrics = @{};
+    
+    [self addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"H:|-0-[spin]-0-|" options:0 metrics:metrics views:views]];
+    [self addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"V:|-100-[spin(100)]" options:0 metrics:metrics views:views]];
+    
+    return self;
+}
+
 @end
