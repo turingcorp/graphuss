ddb241519b88cfba85c8118ddaafff2af1eb7a4a
vauxhall <zero.griffin@gmail.com>
2016-01-30 22:23:39 -0600

    fitler

--- graphuss/sourceteq/source/m/graphics/mgraphicsfiltercolorred.m
+++ graphuss/sourceteq/source/m/graphics/mgraphicsfiltercolorred.m
@@ -11,8 +11,23 @@
     uint red = pixel.red255;
     uint green = pixel.green255;
     uint blue = pixel.blue255;
+    uint delta = 20;
     
-    
+    if(red > green)
+    {
+        if(red > blue)
+        {
+            if(green > blue)
+            {
+                red += delta;
+                
+                if(red > 255)
+                {
+                    red = 255;
+                }
+            }
+        }
+    }
     
     newcolor = [mgraphicspixel intfromintsred:red green:green blue:blue];
     
