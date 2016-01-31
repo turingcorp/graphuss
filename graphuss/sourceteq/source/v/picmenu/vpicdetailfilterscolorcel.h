ec5cc3b02a57a5628cfb8b6c700a4c467995f75b
vauxhall <zero.griffin@gmail.com>
2016-01-30 21:23:13 -0600

    cel

--- graphuss/sourceteq/source/v/picmenu/vpicdetailfilterscolorcel.h
+++ graphuss/sourceteq/source/v/picmenu/vpicdetailfilterscolorcel.h
@@ -1,5 +1,11 @@
 #import "appdel.h"
+#import "mpicmenufilterscolorprotocol.h"
 
 @interface vpicdetailfilterscolorcel:UICollectionViewCell
+
+-(void)config:(id<mpicmenufilterscolorprotocol>)model;
+
+@property(weak, nonatomic)UIImageView *image;
+@property(weak, nonatomic)UIView *circle;
 
 @end
--- graphuss/sourceteq/source/v/picmenu/vpicdetailfilterscolorcel.m
+++ graphuss/sourceteq/source/v/picmenu/vpicdetailfilterscolorcel.m
@@ -8,7 +8,60 @@
     [self setClipsToBounds:YES];
     [self setBackgroundColor:[UIColor clearColor]];
     
+    NSUInteger celside = frame.size.width;
+    NSUInteger circleside = celside - 10;
+    CGFloat radius = circleside / 2.0;
+    
+    UIView *circle = [[UIView alloc] init];
+    [circle setClipsToBounds:YES];
+    [circle setUserInteractionEnabled:NO];
+    [circle setTranslatesAutoresizingMaskIntoConstraints:NO];
+    [circle.layer setCornerRadius:radius];
+    self.circle = circle;
+    
+    UIImageView *image = [[UIImageView alloc] init];
+    [image setClipsToBounds:YES];
+    [image setUserInteractionEnabled:NO];
+    [image setContentMode:UIViewContentModeScaleAspectFill];
+    self.image = image;
+    
+    [self addSubview:circle];
+    [self addSubview:image];
+    
     return self;
+}
+
+-(void)setSelected:(BOOL)selected
+{
+    [super setSelected:selected];
+    [self hover];
+}
+
+-(void)setHighlighted:(BOOL)highlighted
+{
+    [super setHighlighted:highlighted];
+    [self hover];
+}
+
+#pragma mark functionality
+
+-(void)hover
+{
+    if(self.isSelected || self.isHighlighted)
+    {
+        [self.circle setBackgroundColor:colormain];
+    }
+    else
+    {
+        [self.circle setBackgroundColor:[UIColor whiteColor]];
+    }
+}
+
+#pragma mark public
+
+-(void)config:(id<mpicmenufilterscolorprotocol>)model
+{
+    [self hover];
 }
 
 @end
