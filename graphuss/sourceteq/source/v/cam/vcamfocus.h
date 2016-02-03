cfdefc60c592aab2459bcccd3c093be7b19d33bb
vauxhall <zero.griffin@gmail.com>
2016-02-03 17:40:45 -0600

    slider

--- graphuss/sourceteq/source/v/cam/vcamfocus.h
+++ graphuss/sourceteq/source/v/cam/vcamfocus.h
@@ -2,4 +2,8 @@
 
 @interface vcamfocus:UIView
 
+@property(weak, nonatomic)UISlider *slider;
+@property(weak, nonatomic)UILabel *labelmin;
+@property(weak, nonatomic)UILabel *labelmax;
+
 @end
--- graphuss/sourceteq/source/v/cam/vcamfocus.m
+++ graphuss/sourceteq/source/v/cam/vcamfocus.m
@@ -22,12 +22,41 @@
     UISwitch *focusswitch = [[UISwitch alloc] init];
     [focusswitch setOnTintColor:colormain];
     [focusswitch setTranslatesAutoresizingMaskIntoConstraints:NO];
+    [focusswitch addTarget:self action:@selector(actionswitch:) forControlEvents:UIControlEventValueChanged];
+    
+    UISlider *slider = [[UISlider alloc] init];
+    [slider setMinimumTrackTintColor:colormain];
+    [slider setMaximumTrackTintColor:[UIColor colorWithWhite:0 alpha:0.2]];
+    [slider setTranslatesAutoresizingMaskIntoConstraints:NO];
+    self.slider = slider;
+    
+    UILabel *labelmin = [[UILabel alloc] init];
+    [labelmin setBackgroundColor:[UIColor clearColor]];
+    [labelmin setUserInteractionEnabled:NO];
+    [labelmin setTranslatesAutoresizingMaskIntoConstraints:NO];
+    [labelmin setFont:[UIFont fontWithName:fontname size:15]];
+    [labelmin setTextColor:[UIColor colorWithWhite:0 alpha:0.7]];
+    [labelmin setText:NSLocalizedString(@"cam_menu_item_focus_label_min", nil)];
+    self.labelmin = labelmin;
+    
+    UILabel *labelmax = [[UILabel alloc] init];
+    [labelmax setBackgroundColor:[UIColor clearColor]];
+    [labelmax setUserInteractionEnabled:NO];
+    [labelmax setTranslatesAutoresizingMaskIntoConstraints:NO];
+    [labelmax setFont:[UIFont fontWithName:fontname size:15]];
+    [labelmax setTextColor:[UIColor colorWithWhite:0 alpha:0.7]];
+    [labelmax setTextAlignment:NSTextAlignmentRight];
+    [labelmax setText:NSLocalizedString(@"cam_menu_item_focus_label_max", nil)];
+    self.labelmax = labelmax;
     
     [self addSubview:blur];
     [self addSubview:label];
     [self addSubview:focusswitch];
+    [self addSubview:labelmin];
+    [self addSubview:labelmax];
+    [self addSubview:slider];
     
-    NSDictionary *views = @{@"blur":blur, @"label":label, @"switch":focusswitch};
+    NSDictionary *views = @{@"blur":blur, @"label":label, @"switch":focusswitch, @"slider":slider, @"min":labelmin, @"max":labelmax};
     NSDictionary *metrics = @{};
     
     [self addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"H:|-0-[blur]-0-|" options:0 metrics:metrics views:views]];
@@ -35,8 +64,44 @@
     [self addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"H:|-20-[label]-12-[switch]" options:0 metrics:metrics views:views]];
     [self addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"V:[label]-90-|" options:0 metrics:metrics views:views]];
     [self addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"V:[switch]-85-|" options:0 metrics:metrics views:views]];
+    [self addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"H:|-20-[slider]-20-|" options:0 metrics:metrics views:views]];
+    [self addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"V:[slider]-20-|" options:0 metrics:metrics views:views]];
+    [self addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"H:|-20-[min]" options:0 metrics:metrics views:views]];
+    [self addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"H:[max]-20-|" options:0 metrics:metrics views:views]];
+    [self addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"V:[min]-50-|" options:0 metrics:metrics views:views]];
+    [self addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"V:[max]-50-|" options:0 metrics:metrics views:views]];
     
     return self;
+}
+
+#pragma mark actions
+
+-(void)actionswitch:(UISwitch*)aswitch
+{
+    if(aswitch.isOn)
+    {
+        [self hideslider];
+    }
+    else
+    {
+        [self showslider];
+    }
+}
+
+#pragma mark functionality
+
+-(void)showslider
+{
+    [self.slider setHidden:YES];
+    [self.labelmin setHidden:YES];
+    [self.labelmax setHidden:YES];
+}
+
+-(void)hideslider
+{
+    [self.slider setHidden:NO];
+    [self.labelmin setHidden:NO];
+    [self.labelmax setHidden:NO];
 }
 
 @end
