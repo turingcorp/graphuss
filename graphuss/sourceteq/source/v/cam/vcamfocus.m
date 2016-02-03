cc7a20bb907449c0497dd6f86752f66cf77a1573
vauxhall <zero.griffin@gmail.com>
2016-02-03 16:48:54 -0600

    switch

--- graphuss/sourceteq/source/v/cam/vcamfocus.m
+++ graphuss/sourceteq/source/v/cam/vcamfocus.m
@@ -16,19 +16,25 @@
     [label setBackgroundColor:[UIColor clearColor]];
     [label setTranslatesAutoresizingMaskIntoConstraints:NO];
     [label setFont:[UIFont fontWithName:fontname size:16]];
-    [label setTextColor:[UIColor colorWithWhite:0 alpha:0.7]];
+    [label setTextColor:[UIColor blackColor]];
     [label setText:NSLocalizedString(@"cam_menu_item_focus_label", nil)];
+    
+    UISwitch *focusswitch = [[UISwitch alloc] init];
+    [focusswitch setOnTintColor:colormain];
+    [focusswitch setTranslatesAutoresizingMaskIntoConstraints:NO];
     
     [self addSubview:blur];
     [self addSubview:label];
+    [self addSubview:focusswitch];
     
-    NSDictionary *views = @{@"blur":blur, @"label":label};
+    NSDictionary *views = @{@"blur":blur, @"label":label, @"switch":focusswitch};
     NSDictionary *metrics = @{};
     
     [self addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"H:|-0-[blur]-0-|" options:0 metrics:metrics views:views]];
     [self addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"V:[blur(130)]-0-|" options:0 metrics:metrics views:views]];
-    [self addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"H:|-20-[label]" options:0 metrics:metrics views:views]];
+    [self addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"H:|-20-[label]-12-[switch]" options:0 metrics:metrics views:views]];
     [self addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"V:[label]-90-|" options:0 metrics:metrics views:views]];
+    [self addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"V:[switch]-85-|" options:0 metrics:metrics views:views]];
     
     return self;
 }
