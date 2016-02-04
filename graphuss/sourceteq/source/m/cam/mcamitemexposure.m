6abf0033a599f260155919a28f972cc88930a91a
vauxhall <zero.griffin@gmail.com>
2016-02-04 09:26:02 -0600

    flash

--- graphuss/sourceteq/source/m/cam/mcamitemexposure.m
+++ graphuss/sourceteq/source/m/cam/mcamitemexposure.m
@@ -11,4 +11,21 @@
     return self;
 }
 
+#pragma mark public
+
+-(void)selected:(vcammenuoptions*)options
+{
+    [super selected:options];
+    
+    vcamfocus *focus = [[vcamfocus alloc] init:(ccam*)options.viewcam.controller];
+    options.overview = focus;
+    [options addSubview:focus];
+    
+    NSDictionary *views = @{@"focus":focus};
+    NSDictionary *metrics = @{};
+    
+    [options addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"H:|-0-[focus]-0-|" options:0 metrics:metrics views:views]];
+    [options addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"V:|-0-[focus]-50-|" options:0 metrics:metrics views:views]];
+}
+
 @end
--- graphuss/sourceteq/source/m/cam/mcamitemflash.m
+++ graphuss/sourceteq/source/m/cam/mcamitemflash.m
@@ -2,4 +2,30 @@
 
 @implementation mcamitemflash
 
+-(instancetype)init
+{
+    self = [super init];
+    
+    self.title = NSLocalizedString(@"cam_menu_item_exposure", nil);
+    
+    return self;
+}
+
+#pragma mark public
+
+-(void)selected:(vcammenuoptions*)options
+{
+    [super selected:options];
+    
+    vcamfocus *focus = [[vcamfocus alloc] init:(ccam*)options.viewcam.controller];
+    options.overview = focus;
+    [options addSubview:focus];
+    
+    NSDictionary *views = @{@"focus":focus};
+    NSDictionary *metrics = @{};
+    
+    [options addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"H:|-0-[focus]-0-|" options:0 metrics:metrics views:views]];
+    [options addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"V:|-0-[focus]-50-|" options:0 metrics:metrics views:views]];
+}
+
 @end
