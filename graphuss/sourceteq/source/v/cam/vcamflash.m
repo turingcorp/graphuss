585575b5a6507d3367455b7766e94a06b6a66367
vauxhall <zero.griffin@gmail.com>
2016-02-04 14:50:05 -0600

    collection

--- graphuss/sourceteq/source/v/cam/vcamflash.m
+++ graphuss/sourceteq/source/v/cam/vcamflash.m
@@ -11,12 +11,45 @@
     
     self.controller = controller;
     self.model = [[mcamflash alloc] init];
+ 
+    UICollectionViewFlowLayout *flow = [[UICollectionViewFlowLayout alloc] init];
+    [flow setHeaderReferenceSize:CGSizeZero];
+    [flow setFooterReferenceSize:CGSizeZero];
+    [flow setMinimumInteritemSpacing:0];
+    [flow setMinimumLineSpacing:0];
+    [flow setScrollDirection:UICollectionViewScrollDirectionHorizontal];
+    [flow setSectionInset:UIEdgeInsetsZero];
+    
+    UICollectionView *collection = [[UICollectionView alloc] initWithFrame:CGRectZero collectionViewLayout:flow];
+    [collection setClipsToBounds:YES];
+    [collection setBackgroundColor:[UIColor clearColor]];
+    [collection setScrollEnabled:NO];
+    [collection setBounces:NO];
+    [collection registerClass:[vcamflashcel class] forCellWithReuseIdentifier:celid];
+    [collection setDelegate:self];
+    [collection setDataSource:self];
+    [collection setTranslatesAutoresizingMaskIntoConstraints:NO];
+    
+    [self addSubview:collection];
+    
+    NSDictionary *views = @{@"col":collection};
+    NSDictionary *metrics = @{};
+    
+    [self addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"H:|-0-[col]-0-|" options:0 metrics:metrics views:views]];
+    [self addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"V:[col]-60-|" options:0 metrics:metrics views:views]];
     
     return self;
 }
 
 #pragma mark -
 #pragma mark col del
+
+-(CGSize)collectionView:(UICollectionView*)col layout:(UICollectionViewLayout*)layout sizeForItemAtIndexPath:(NSIndexPath*)index
+{
+    CGSize size = CGSizeMake(col.bounds.size.width / [self.model count], col.bounds.size.height);
+    
+    return size;
+}
 
 -(NSInteger)numberOfSectionsInCollectionView:(UICollectionView*)col
 {
