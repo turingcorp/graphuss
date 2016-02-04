d8bbf3b9302a5a0740469df5ce3538b560812206
vauxhall <zero.griffin@gmail.com>
2016-02-04 17:23:25 -0600

    config protocol

--- graphuss/sourceteq/source/m/config/mconfigabout.h
+++ graphuss/sourceteq/source/m/config/mconfigabout.h
@@ -1,13 +1,6 @@
-//
-//  mconfigabout.h
-//  graphuss
-//
-//  Created by zero on 2/4/16.
-//  Copyright © 2016 Iturbide. All rights reserved.
-//
+#import "appdel.h"
+#import "mconfigprotocol.h"
 
-#import <Foundation/Foundation.h>
-
-@interface mconfigabout : NSObject
+@interface mconfigabout:NSObject<mconfigprotocol>
 
 @end
--- graphuss/sourceteq/source/m/config/mconfigabout.m
+++ graphuss/sourceteq/source/m/config/mconfigabout.m
@@ -1,11 +1,3 @@
-//
-//  mconfigabout.m
-//  graphuss
-//
-//  Created by zero on 2/4/16.
-//  Copyright © 2016 Iturbide. All rights reserved.
-//
-
 #import "mconfigabout.h"
 
 @implementation mconfigabout
