2c2dc35794b2f197abbe7baadef04f5fe3ba8eac
vauxhall <zero.griffin@gmail.com>
2016-02-04 14:26:06 -0600

    items

--- graphuss/sourceteq/source/m/cam/mcamflash.m
+++ graphuss/sourceteq/source/m/cam/mcamflash.m
@@ -5,6 +5,17 @@
     NSArray *array;
 }
 
+-(instancetype)init
+{
+    self = [super init];
+    
+    array = [NSArray arrayWithObjects:
+             [mcamitemflashof],
+             nil];
+    
+    return self;
+}
+
 #pragma mark public
 
 -(NSUInteger)count
