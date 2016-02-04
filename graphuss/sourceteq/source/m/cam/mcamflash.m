bb0886bf91db727c0248f67af83f66ec24e74c7c
vauxhall <zero.griffin@gmail.com>
2016-02-04 14:10:00 -0600

    flash item

---
+++ graphuss/sourceteq/source/m/cam/mcamflash.m
@@ -0,0 +1,13 @@
+//
+//  mcamflash.m
+//  graphuss
+//
+//  Created by zero on 2/4/16.
+//  Copyright © 2016 Iturbide. All rights reserved.
+//
+
+#import "mcamflash.h"
+
+@implementation mcamflash
+
+@end
---
+++ graphuss/sourceteq/source/m/cam/mcamflashitem.m
@@ -0,0 +1,13 @@
+//
+//  mcamflashitem.m
+//  graphuss
+//
+//  Created by zero on 2/4/16.
+//  Copyright © 2016 Iturbide. All rights reserved.
+//
+
+#import "mcamflashitem.h"
+
+@implementation mcamflashitem
+
+@end
