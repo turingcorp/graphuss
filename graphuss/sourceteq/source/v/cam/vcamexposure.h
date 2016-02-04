3434f40abe418a03176c8b8709d7380f50572329
vauxhall <zero.griffin@gmail.com>
2016-02-04 11:17:04 -0600

    exposure

--- graphuss/sourceteq/source/v/cam/vcamexposure.h
+++ graphuss/sourceteq/source/v/cam/vcamexposure.h
@@ -6,8 +6,14 @@
 @interface vcamexposure:UIView
 
 -(instancetype)init:(ccam*)controller;
+-(void)updateexposure;
 
 @property(weak, nonatomic)ccam *controller;
 @property(weak, nonatomic)vcamexposureiso *iso;
+@property(weak, nonatomic)UISlider *slider;
+@property(weak, nonatomic)UILabel *labelduration;
+@property(weak, nonatomic)UILabel *labeldurationmin;
+@property(weak, nonatomic)UILabel *labeldurationmax;
+@property(weak, nonatomic)UILabel *labeliso;
 
 @end
--- graphuss/sourceteq/source/v/cam/vcamexposure.m
+++ graphuss/sourceteq/source/v/cam/vcamexposure.m
@@ -1,6 +1,9 @@
 #import "vcamexposure.h"
 
 @implementation vcamexposure
+{
+    NSTimer *timer;
+}
 
 -(instancetype)init:(ccam*)controller
 {
@@ -48,11 +51,92 @@
     return self;
 }
 
+-(void)dealloc
+{
+    if(timer)
+    {
+        [self timeout:timer];
+    }
+}
+
 #pragma mark actions
 
 -(void)actionswitch:(UISwitch*)aswitch
 {
+    BOOL autofocus = aswitch.isOn;
+    [[mcamsettings singleton] focusauto:autofocus];
     
+    if(autofocus)
+    {
+        [self hidecontrols];
+    }
+    else
+    {
+        [self showcontrols];
+    }
+    
+    [self updateexposure];
+}
+
+-(void)actionslider:(UISlider*)slider
+{
+//    [[mcamsettings singleton] focusamount:slider.value];
+    
+    [self updateexposure];
+}
+
+#pragma mark functionality
+
+-(void)timeout:(NSTimer*)atimer
+{
+    [timer invalidate];
+    timer = nil;
+    
+    if([mcamsettings singleton].focusautomatic)
+    {
+        [[analytics singleton] trackevent:ga_event_cam_focus action:ga_action_automatic label:nil];
+    }
+    else
+    {
+        NSString *label = [[tools singleton] numbertostring:@(self.slider.value)];
+        
+        [[analytics singleton] trackevent:ga_event_cam_focus action:ga_action_manual label:label];
+    }
+}
+
+-(void)showcontrols
+{
+    [self.slider setHidden:NO];
+    [self.labelmin setHidden:NO];
+    [self.labelmax setHidden:NO];
+}
+
+-(void)hidecontrols
+{
+    [self.slider setHidden:YES];
+    [self.labelmin setHidden:YES];
+    [self.labelmax setHidden:YES];
+}
+
+#pragma mark public
+
+-(void)updateexposure
+{
+    [timer invalidate];
+    timer = [NSTimer scheduledTimerWithTimeInterval:10 target:self selector:@selector(timeout:) userInfo:nil repeats:NO];
+    
+    CGFloat focusamount;
+    
+    if([mcamsettings singleton].focusautomatic)
+    {
+        focusamount = -1;
+    }
+    else
+    {
+        focusamount = self.slider.value;
+    }
+    
+    [self.controller changefocus:focusamount];
 }
 
 @end
