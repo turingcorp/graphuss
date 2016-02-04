f18280c0707ab7d7216f642c1bb01d2259cfaa70
vauxhall <zero.griffin@gmail.com>
2016-02-04 16:44:15 -0600

    protocol

--- graphuss/sourceteq/source/m/config/mconfig.h
+++ graphuss/sourceteq/source/m/config/mconfig.h
@@ -1,13 +1,9 @@
-//
-//  mconfig.h
-//  graphuss
-//
-//  Created by zero on 2/4/16.
-//  Copyright © 2016 Iturbide. All rights reserved.
-//
+#import "appdel.h"
+#import "mconfigprotocol.h"
 
-#import <Foundation/Foundation.h>
+@interface mconfig:NSObject
 
-@interface mconfig : NSObject
+-(NSUInteger)count;
+-(id<mconfigprotocol>)item:(NSUInteger)index;
 
 @end
--- graphuss/sourceteq/source/m/config/mconfig.m
+++ graphuss/sourceteq/source/m/config/mconfig.m
@@ -1,13 +1,24 @@
-//
-//  mconfig.m
-//  graphuss
-//
-//  Created by zero on 2/4/16.
-//  Copyright © 2016 Iturbide. All rights reserved.
-//
-
 #import "mconfig.h"
 
 @implementation mconfig
+{
+    NSArray *array;
+}
+
+#pragma mark public
+
+-(NSUInteger)count
+{
+    NSUInteger count = array.count;
+    
+    return count;
+}
+
+-(id<mconfigprotocol>)item:(NSUInteger)index;
+{
+    id<mconfigprotocol> item = array[index];
+    
+    return item;
+}
 
 @end
