6906e7e59562777d6ee0c21162d0d9f98979e6a1
vauxhall <zero.griffin@gmail.com>
2016-02-04 14:23:23 -0600

    cam items

--- graphuss/sourceteq/source/m/cam/mcamflash.h
+++ graphuss/sourceteq/source/m/cam/mcamflash.h
@@ -1,5 +1,10 @@
 #import "appdel.h"
 
+@class mcamitem;
+
 @interface mcamflash:NSObject
+
+-(NSUInteger)count;
+-(mcamitem*)item:(NSUInteger)index;
 
 @end
--- graphuss/sourceteq/source/m/cam/mcamflash.m
+++ graphuss/sourceteq/source/m/cam/mcamflash.m
@@ -1,5 +1,24 @@
 #import "mcamflash.h"
 
 @implementation mcamflash
+{
+    NSArray *array;
+}
+
+#pragma mark public
+
+-(NSUInteger)count
+{
+    NSUInteger count = array.count;
+    
+    return count;
+}
+
+-(mcamitem*)item:(NSUInteger)index;
+{
+    mcamitem *item = array[index];
+    
+    return item;
+}
 
 @end
