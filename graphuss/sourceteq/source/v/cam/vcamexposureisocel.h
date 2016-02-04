ea548918f7a3b966e506d5b293782208d0ed44e8
vauxhall <zero.griffin@gmail.com>
2016-02-04 09:58:47 -0600

    exposure iso cel

---
+++ graphuss/sourceteq/source/v/cam/vcamexposureisocel.h
@@ -0,0 +1,13 @@
+//
+//  vcamexposureisocel.h
+//  graphuss
+//
+//  Created by zero on 2/4/16.
+//  Copyright © 2016 Iturbide. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface vcamexposureisocel : NSObject
+
+@end
---
+++ graphuss/sourceteq/source/v/cam/vcamexposureisocel.m
@@ -0,0 +1,13 @@
+//
+//  vcamexposureisocel.m
+//  graphuss
+//
+//  Created by zero on 2/4/16.
+//  Copyright © 2016 Iturbide. All rights reserved.
+//
+
+#import "vcamexposureisocel.h"
+
+@implementation vcamexposureisocel
+
+@end
