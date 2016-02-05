60fb772be117820f36d783f440c37c0766c61a21
vauxhall <zero.griffin@gmail.com>
2016-02-05 10:18:08 -0600

    config

--- graphuss/sourceteq/source/v/config/vconfigcontact.m
+++ graphuss/sourceteq/source/v/config/vconfigcontact.m
@@ -16,6 +16,13 @@
 #pragma mark -
 #pragma mark col del
 
+-(CGSize)collectionView:(UICollectionView*)col layout:(UICollectionViewLayout*)layout sizeForItemAtIndexPath:(NSIndexPath*)index
+{
+    CGSize size = CGSizeMake(col.bounds.size.width, 60);
+    
+    return size;
+}
+
 -(NSInteger)numberOfSectionsInCollectionView:(UICollectionView*)col
 {
     return 1;
@@ -28,7 +35,10 @@
 
 -(UICollectionViewCell*)collectionView:(UICollectionView*)col cellForItemAtIndexPath:(NSIndexPath*)index
 {
+    vconfigcontactcel *cel = [col dequeueReusableCellWithReuseIdentifier:celid forIndexPath:index];
+    [cel config:[self.controllercontact.model item:index.item]];
     
+    return cel;
 }
 
 @end
