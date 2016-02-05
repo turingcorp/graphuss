e2dd9749791d9e82963d1a4aab3b0eb930102230
vauxhall <zero.griffin@gmail.com>
2016-02-05 10:44:15 -0600

    field base

--- graphuss/sourceteq/source/v/config/vconfigcontact.h
+++ graphuss/sourceteq/source/v/config/vconfigcontact.h
@@ -6,5 +6,9 @@
 @interface vconfigcontact:viewbase<UICollectionViewDataSource, UICollectionViewDelegate, UICollectionViewDelegateFlowLayout>
 
 @property(weak, nonatomic)cconfigcontact *controllercontact;
+@property(weak, nonatomic)UIView *fieldbase;
+@property(weak, nonatomic)UIButton *buttoncancel;
+@property(weak, nonatomic)UIButton *buttonsend;
+@property(weak, nonatomic)UITextField *field;
 
 @end
--- graphuss/sourceteq/source/v/config/vconfigcontact.m
+++ graphuss/sourceteq/source/v/config/vconfigcontact.m
@@ -29,13 +29,56 @@
     [collection registerClass:[vconfigcontactcel class] forCellWithReuseIdentifier:celid];
     [collection setTranslatesAutoresizingMaskIntoConstraints:NO];
     
-    [self addSubview:collection];
+    UITextField *field = [[UITextField alloc] init];
+    [field setAutocapitalizationType:UITextAutocapitalizationTypeNone];
+    [field setAutocorrectionType:UITextAutocorrectionTypeNo];
+    [field setBackgroundColor:[UIColor clearColor]];
+    [field setBorderStyle:UITextBorderStyleNone];
+    [field setClearButtonMode:UITextFieldViewModeNever];
+    [field setFont:[UIFont fontWithName:fontname size:18]];
+    [field setKeyboardAppearance:UIKeyboardAppearanceLight];
+    [field setKeyboardType:UIKeyboardTypeAlphabet];
+    [field setReturnKeyType:UIReturnKeyDone];
+    [field setSpellCheckingType:UITextSpellCheckingTypeNo];
+    [field setTextColor:[UIColor blackColor]];
+    [field setTintColor:[UIColor blackColor]];
+    [field setTranslatesAutoresizingMaskIntoConstraints:NO];
+    [field setHidden:YES];
+    self.field = field;
     
-    NSDictionary *views = @{@"col":collection};
+    UIView *fieldbase = [[UIView alloc] init];
+    [fieldbase setClipsToBounds:YES];
+    [fieldbase setBackgroundColor:[UIColor clearColor]];
+    [fieldbase setTranslatesAutoresizingMaskIntoConstraints:NO];
+    
+    vblur *blur = [vblur light:NO];
+    
+    UIButton *buttonsend = [[UIButton alloc] init];
+    [buttonsend setBackgroundColor:colorsecond];
+    [buttonsend setClipsToBounds:YES];
+    [buttonsend setTranslatesAutoresizingMaskIntoConstraints:NO];
+    self.buttonsend = buttonsend;
+    
+    UIButton *buttoncancel = [[UIButton alloc] init];
+    [buttoncancel setBackgroundColor:colormain];
+    [buttoncancel setClipsToBounds:YES];
+    [buttoncancel setTranslatesAutoresizingMaskIntoConstraints:NO];
+    self.buttoncancel = buttoncancel;
+    
+    [fieldbase addSubview:blur];
+    [fieldbase addSubview:buttoncancel];
+    [fieldbase addSubview:buttonsend];
+    [fieldbase addSubview:field];
+    
+    [self addSubview:collection];
+    [self addSubview:fieldbase];
+    
+    NSDictionary *views = @{@"col":collection, @"field":field, @"fieldbase":fieldbase, @"blur":blur};
     NSDictionary *metrics = @{};
     
     [self addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"H:|-0-[col]-0-|" options:0 metrics:metrics views:views]];
     [self addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"V:|-0-[col]-0-|" options:0 metrics:metrics views:views]];
+    [self addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"H:|-0-[field]-0-|" options:0 metrics:metrics views:views]];
     
     return self;
 }
