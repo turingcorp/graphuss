e743491eb00b547a81a19d44c8adca21c40f3f88
vauxhall <zero.griffin@gmail.com>
2016-02-05 08:38:31 -0600

    delete

--- graphuss/sourceteq/source/v/config/vconfiglibrarydelete.h
+++ graphuss/sourceteq/source/v/config/vconfiglibrarydelete.h
@@ -5,5 +5,8 @@
 @interface vconfiglibrarydelete:UIView
 
 @property(weak, nonatomic)vspinner *spinner;
+@property(weak, nonatomic)UILabel *labeldescr;
+@property(weak, nonatomic)UILabel *labelfinalize;
+@property(weak, nonatomic)UIButton *button;
 
 @end
--- graphuss/sourceteq/source/v/config/vconfiglibrarydelete.m
+++ graphuss/sourceteq/source/v/config/vconfiglibrarydelete.m
@@ -11,12 +11,38 @@
     vspinner *spinner = [[vspinner alloc] init];
     self.spinner = spinner;
     
+    UILabel *labeldescr = [[UILabel alloc] init];
+    [labeldescr setBackgroundColor:[UIColor clearColor]];
+    [labeldescr setUserInteractionEnabled:NO];
+    [labeldescr setTranslatesAutoresizingMaskIntoConstraints:NO];
+    [labeldescr setTextAlignment:NSTextAlignmentCenter];
+    [labeldescr setNumberOfLines:0];
+    [labeldescr setFont:[UIFont fontWithName:fontname size:15]];
+    [labeldescr setTextColor:colormain];
+    [labeldescr setText:NSLocalizedString(@"config_library_disk_descr", nil)];
+    self.labeldescr = labeldescr;
+    
+    UILabel *labelfinalize = [[UILabel alloc] init];
+    [labelfinalize setText:NSLocalizedString(@"config_library_disk_finalize", nil)];
+    self.labelfinalize = labelfinalize;
+    
+    UIButton *button = [[UIButton alloc] init];
+    self.button = button;
+    
     [self addSubview:spinner];
-    NSDictionary *views = @{@"spinner":spinner};
+    [self addSubview:labeldescr];
+    [self addSubview:labelfinalize];
+    [self addSubview:button];
+    
+    NSDictionary *views = @{@"spinner":spinner, @"descr":labeldescr, @"finalize":labelfinalize, @"button":button};
     NSDictionary *metrics = @{};
     
     [self addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"H:|-0-[spinner]-0-|" options:0 metrics:metrics views:views]];
-    [self addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"V:|-150-[spinner(80)]" options:0 metrics:metrics views:views]];
+    [self addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"V:|-150-[spinner(80)]-20-[descr]" options:0 metrics:metrics views:views]];
+    [self addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"H:|-20-[descr]-20-|" options:0 metrics:metrics views:views]];
+    [self addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"H:|-50-[finalize]-50-|" options:0 metrics:metrics views:views]];
+    [self addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"H:|-50-[button]-50-|" options:0 metrics:metrics views:views]];
+    [self addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"V:|-150-[finalize]-20-[button(36)]" options:0 metrics:metrics views:views]];
     
     return self;
 }
