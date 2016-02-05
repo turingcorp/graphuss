d7709f343983118eb62083502822d50bf33b0c75
vauxhall <zero.griffin@gmail.com>
2016-02-05 09:21:46 -0600

    config about

---
+++ graphuss/sourceteq/source/c/config/cconfigabout.h
@@ -0,0 +1,5 @@
+#import "appdel.h"
+
+@interface cconfigabout:UIViewController
+
+@end
---
+++ graphuss/sourceteq/source/c/config/cconfigabout.m
@@ -0,0 +1,17 @@
+#import "cconfigabout.h"
+
+@implementation cconfigabout
+
+-(void)viewDidLoad
+{
+    [super viewDidLoad];
+    [self setTitle:NSLocalizedString(@"config_library_title", nil)];
+    [[analytics singleton] trackscreen:ga_screen_config_library];
+}
+
+-(void)loadView
+{
+    self.view = [[vconfiglibrary alloc] init:self];
+}
+
+@end
