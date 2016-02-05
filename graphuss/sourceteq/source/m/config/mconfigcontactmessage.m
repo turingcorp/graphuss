929e033f7b1e8d4d3ec4ddb90ed5afa406bd52d0
vauxhall <zero.griffin@gmail.com>
2016-02-05 10:07:50 -0600

    config contact

--- graphuss/sourceteq/source/m/config/mconfigcontactmessage.m
+++ graphuss/sourceteq/source/m/config/mconfigcontactmessage.m
@@ -2,4 +2,19 @@
 
 @implementation mconfigcontactmessage
 
+#pragma mark -
+#pragma mark contact protocol
+
+-(NSString*)title
+{
+    NSString *title = NSLocalizedString(@"config_contact_message_title", nil);
+    
+    return title;
+}
+
+-(void)selected:(vconfigcontact*)view
+{
+    
+}
+
 @end
