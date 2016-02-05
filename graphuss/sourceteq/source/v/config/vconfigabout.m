69cd822141c8843c72d77984654752cc802918d3
vauxhall <zero.griffin@gmail.com>
2016-02-05 09:41:14 -0600

    wording

--- graphuss/sourceteq/source/v/config/vconfigabout.m
+++ graphuss/sourceteq/source/v/config/vconfigabout.m
@@ -5,6 +5,35 @@
 -(instancetype)init:(cconfigabout*)controller
 {
     self = [super init:controller];
+    [self setBackgroundColor:[UIColor whiteColor]];
+    
+    NSDictionary *attrtitle = @{NSForegroundColorAttributeName:colormain, NSFontAttributeName:[UIFont fontWithName:fontboldname size:18]};
+    NSDictionary *attrversion = @{NSForegroundColorAttributeName:colormain, NSFontAttributeName:[UIFont fontWithName:fontname size:15]};
+    NSDictionary *attrcopy = @{NSForegroundColorAttributeName:[UIColor blackColor], NSFontAttributeName:[UIFont fontWithName:fontname size:12]};
+    
+    NSAttributedString *strtitle = [[NSAttributedString alloc] initWithString:NSLocalizedString(@"config_about_in_title", nil) attributes:attrtitle];
+    NSAttributedString *strversion = [[NSAttributedString alloc] initWithString:[NSString stringWithFormat:NSLocalizedString(@"config_about_in_descr", nil), [[NSBundle mainBundle] infoDictionary][@"CFBundleShortVersionString"]] attributes:attrversion];
+    NSAttributedString *strcopy = [[NSAttributedString alloc] initWithString:NSLocalizedString(@"config_about_in_copy", nil) attributes:attrcopy];
+    
+    NSMutableAttributedString *mut = [[NSMutableAttributedString alloc] init];
+    [mut appendAttributedString:strtitle];
+    [mut appendAttributedString:strversion];
+    [mut appendAttributedString:strcopy];
+    
+    UILabel *label = [[UILabel alloc] init];
+    [label setAttributedText:mut];
+    [label setUserInteractionEnabled:NO];
+    [label setTextAlignment:NSTextAlignmentCenter];
+    [label setTranslatesAutoresizingMaskIntoConstraints:NO];
+    [label setNumberOfLines:0];
+    
+    [self addSubview:label];
+    
+    NSDictionary *views = @{@"label":label};
+    NSDictionary *metrics = @{};
+    
+    [self addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"H:|-20-[label]-20-|" options:0 metrics:metrics views:views]];
+    [self addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"V:|-100-[label]" options:0 metrics:metrics views:views]];
     
     return self;
 }
